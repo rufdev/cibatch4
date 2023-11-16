7f8d8de93027bf13839fdf2f529a5942c37f511c
Rufino John <aguilarufino@gmail.com>
2023-11-16 14:33:36 +0800

    added author migaration

--- app/Database/Migrations/2023-11-16-062614_AuthorsTable.php
+++ app/Database/Migrations/2023-11-16-062614_AuthorsTable.php
@@ -8,11 +8,48 @@ class AuthorsTable extends Migration
 {
     public function up()
     {
-        //
+        $fields = [
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 5,
+                'auto_increment' => true
+            ],
+            'first_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 200,
+                'null' => false
+            ],
+            'last_name' => [
+                'type' => 'VARCHAR',
+                'constraint' => 200,
+                'null' => false
+            ],
+            'email' => [
+                'type' => 'VARCHAR',
+                'constraint' => 200,
+                'null' => false
+            ],
+            'birthdate' => [
+                'type' => 'DATE',
+                'null' => false
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true
+            ],
+        ];
+
+        $this->forge->addField($fields);
+        $this->forge->addPrimaryKey('id');
+        $this->forge->createTable('authors');
     }
 
     public function down()
     {
-        //
+        $this->forge->dropTable('authors');
     }
 }
