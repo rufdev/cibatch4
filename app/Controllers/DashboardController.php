3a00f37e847bae412f715724646cc99640d27457
Rufino John <aguilarufino@gmail.com>
2023-11-17 15:57:35 +0800

    added sample dashboard with chartjs

--- app/Controllers/DashboardController.php
+++ app/Controllers/DashboardController.php
@@ -8,6 +8,18 @@ class DashboardController extends BaseController
 {
     public function index()
     {
-        return view('pages/dashboard');
+        $author = new \App\Models\Author();
+        $post = new \App\Models\Post();
+
+        $data['totalauthors'] = $author->countAll();
+        $data['totalposts'] = $post->countAll();
+
+        $data['barchartdata'] = $author->select("CONCAT(authors.first_name,' ',authors.last_name) as author_name, COUNT(posts.id) as post_count")
+            ->join("posts", "posts.author_id = authors.id")
+            ->groupBy("authors.id")
+            ->findAll();
+
+
+        return view('pages/dashboard', $data);
     }
 }
--- app/Views/pages/dashboard.php
+++ app/Views/pages/dashboard.php
@@ -14,66 +14,33 @@
 <div class="content">
     <div class="container-fluid">
         <div class="row">
-            <div class="col-lg-3 col-6">
+            <div class="col-lg-6 col-6">
                 <!-- small box -->
                 <div class="small-box bg-info">
                     <div class="inner">
-                        <h3>150</h3>
+                        <h3><?= $totalauthors ?></h3>
 
-                        <p>New Orders</p>
+                        <p>Total Authors</p>
                     </div>
                     <div class="icon">
-                        <i class="ion ion-bag"></i>
+                        <i class="ion ion-person"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
             <!-- ./col -->
-            <div class="col-lg-3 col-6">
+            <div class="col-lg-6 col-6">
                 <!-- small box -->
                 <div class="small-box bg-success">
                     <div class="inner">
-                        <h3>53<sup style="font-size: 20px">%</sup></h3>
+                        <h3><?= $totalposts ?></h3>
 
-                        <p>Bounce Rate</p>
+                        <p>Total Posts</p>
                     </div>
                     <div class="icon">
                         <i class="ion ion-stats-bars"></i>
                     </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
                 </div>
             </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-6">
-                <!-- small box -->
-                <div class="small-box bg-warning">
-                    <div class="inner">
-                        <h3>44</h3>
-
-                        <p>User Registrations</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-person-add"></i>
-                    </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-6">
-                <!-- small box -->
-                <div class="small-box bg-danger">
-                    <div class="inner">
-                        <h3>65</h3>
-
-                        <p>Unique Visitors</p>
-                    </div>
-                    <div class="icon">
-                        <i class="ion ion-pie-graph"></i>
-                    </div>
-                    <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-                </div>
-            </div>
-            <!-- ./col -->
         </div>
         <div class="row mb-2">
             <div class="col-md-12">
@@ -102,4 +69,47 @@
         </div>
     </div>
 </div>
+<?= $this->endSection(); ?>
+
+<?= $this->section('pagescripts'); ?>
+<script>
+    $(function () {
+        let barChartCanvas = $('#barChart').get(0).getContext('2d');
+        let barchartdata = <?= json_encode($barchartdata) ?>;
+
+        let barChartData = {
+            labels : barchartdata.map(function(item){
+                return item.author_name;
+            }),
+            datasets: [{
+                label: "Total Post",
+                backgroundColor: "#28a745",
+                borderColor: "#28a745",
+                borderWidth: 1,
+                data: barchartdata.map(function(item){
+                    return item.post_count;
+                }),
+            }]
+        }
+
+        new Chart(barChartCanvas, {
+            type:'bar',
+            data: barChartData,
+            options: {
+                responsive : true,
+                maintainAspectRatio : false,
+                datasetFill: false,
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true,
+                            stepSize: 1
+                        }
+                    }]
+                }
+            }
+        });
+
+    });
+</script>
 <?= $this->endSection(); ?>
